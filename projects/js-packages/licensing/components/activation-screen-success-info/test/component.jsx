99b6f4d1457383dc4fb1f31c86754a67a43de99e
DAVID OKEAMAH <whitneykiss741@gmail.com>
2024-08-15 04:38:26 +0100

    Initialize fix/tiled-gallery-high-volume-crash

---
+++ projects/js-packages/licensing/components/activation-screen-success-info/test/component.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ActivationSuccessInfo from '..';
+import '@testing-library/jest-dom';
+
+describe( 'ActivationSuccessInfo', () => {
+	const testProps = {
+		productId: 2100,
+		siteAdminUrl: 'http://test-site.jurassic.ninja/wp-admin',
+		siteRawUrl: 'http://test-site.jurassic.ninja',
+	};
+
+	describe( 'Render the ActivationSuccessInfo component', () => {
+		it( 'shows the correct product name', () => {
+			render( <ActivationSuccessInfo { ...testProps } /> );
+			expect(
+				screen.getByRole( 'heading', { name: /Jetpack Backup is active!/ } )
+			).toBeInTheDocument();
+		} );
+	} );
+} );

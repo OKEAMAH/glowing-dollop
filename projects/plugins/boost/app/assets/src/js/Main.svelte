92629b8f4c3de03c734d0fd8f942d692406713b7
DAVID OKEAMAH <whitneykiss741@gmail.com>
2024-08-15 04:10:25 +0100

    Initialize test/changelog02

---
+++ projects/plugins/boost/app/assets/src/js/Main.svelte
@@ -0,0 +1,34 @@
+<script>
+	/**
+	 * Internal dependencies
+	 */
+	import Connection from './pages/connection/Connection.svelte';
+	import Settings from './pages/settings/Settings.svelte';
+	import BenefitsInterstitial from './pages/benefits/BenefitsInterstitial.svelte';
+	import Footer from './sections/Footer.svelte';
+	import Header from './sections/Header.svelte';
+	import { connection } from './stores/connection';
+	import config from './stores/config';
+	import { Router, Route } from './utils/router';
+
+	import routerHistory from './utils/router-history';
+</script>
+
+<Router history={routerHistory}>
+	<Route path="upgrade" component={BenefitsInterstitial} />
+	<Route>
+		<div id="jb-settings" class="jb-settings jb-settings--main">
+			<div class="jb-container">
+				<Header />
+			</div>
+
+			{#if $connection.connected || ! config.site.online}
+				<Settings />
+			{:else}
+				<Connection />
+			{/if}
+
+			<Footer />
+		</div>
+	</Route>
+</Router>

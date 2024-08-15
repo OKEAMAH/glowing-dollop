c19dfe96391201abf26058b8e44e085dfe5d0e12
DAVID OKEAMAH <whitneykiss741@gmail.com>
2024-08-15 02:16:47 +0100

    Initialize fusion-sync/ehgy/re48adfb2ccbe5f0d0af3a218bc7be3068c9f3686-wpcom-git-1615895295

---
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/deprecated/index.js
@@ -0,0 +1,11 @@
+/**
+ * Internal Dependencies
+ */
+
+import deprecatedV1 from './v1';
+import deprecatedV2 from './v2';
+import deprecatedV3 from './v3';
+import deprecatedV4 from './v4';
+import deprecatedV5 from './v5';
+
+export default [ deprecatedV1, deprecatedV2, deprecatedV3, deprecatedV4, deprecatedV5 ];

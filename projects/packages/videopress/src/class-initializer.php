2ea578ff7598c74e597c6474ee3e6b1dc5049dd1
DAVID OKEAMAH <whitneykiss741@gmail.com>
2024-08-15 04:56:53 +0100

    Initialize update/change-filter-query-logic

---
+++ projects/packages/videopress/src/class-initializer.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * The initializer class for the videopress package
+ *
+ * @package automattic/jetpack-videopress
+ */
+
+namespace Automattic\Jetpack\VideoPress;
+
+use Automattic\Jetpack\Assets;
+use Automattic\Jetpack\Connection\Initial_State as Connection_Initial_State;
+
+/**
+ * Initialized the VideoPress package
+ */
+class Initializer {
+
+	const JETPACK_VIDEOPRESS_PKG_NAMESPACE = 'jetpack-videopress-pkg';
+
+	/**
+	 * Invoke this method to initialize the VideoPress package
+	 *
+	 * @return void
+	 */
+	public static function init() {
+		if ( ! did_action( 'videopress_init' ) ) {
+
+			self::unconditional_initialization();
+
+			if ( Status::is_active() ) {
+				self::active_initialization();
+			}
+		}
+
+		/**
+		 * Fires after the VideoPress package is initialized
+		 *
+		 * @since 0.1.1
+		 */
+		do_action( 'videopress_init' );
+	}
+
+	/**
+	 * Initialize VideoPress features that should be initialized whenever VideoPress is present, even if the module is not active
+	 *
+	 * @return void
+	 */
+	private static function unconditional_initialization() {
+		require_once __DIR__ . '/utility-functions.php';
+
+		// Set up package version hook.
+		add_filter( 'jetpack_package_versions', __NAMESPACE__ . '\Package_Version::send_package_version_to_tracker' );
+
+		Module_Control::init();
+		new WPCOM_REST_API_V2_Endpoint_VideoPress();
+		if ( is_admin() ) {
+			AJAX::init();
+		}
+	}
+
+	/**
+	 * Initialize VideoPress features that should be initialized only when the module is active
+	 *
+	 * @return void
+	 */
+	private static function active_initialization() {
+		Attachment_Handler::init();
+		Jwt_Token_Bridge::init();
+		self::register_oembed_providers();
+	}
+
+	/**
+	 * Explicitly register VideoPress oembed provider for patterns not supported by core
+	 *
+	 * @return void
+	 */
+	public static function register_oembed_providers() {
+		$host = rawurlencode( home_url() );
+		// videopress.com/v is already registered in core.
+		// By explicitly declaring the provider here, we can speed things up by not relying on oEmbed discovery.
+		wp_oembed_add_provider( '#^https?://video.wordpress.com/v/.*#', 'https://public-api.wordpress.com/oembed/?for=' . $host, true );
+		// This is needed as it's not supported in oEmbed discovery
+		wp_oembed_add_provider( '|^https?://v\.wordpress\.com/([a-zA-Z\d]{8})(.+)?$|i', 'https://public-api.wordpress.com/oembed/?for=' . $host, true ); // phpcs:ignore WordPress.WP.CapitalPDangit.Misspelled
+	}
+
+	/**
+	 * Enqueue plugin admin scripts and styles.
+	 */
+	public static function enqueue_admin_scripts() {
+		Assets::register_script(
+			self::JETPACK_VIDEOPRESS_PKG_NAMESPACE,
+			'../build/admin/index.js',
+			__FILE__,
+			array(
+				'in_footer'  => true,
+				'textdomain' => 'jetpack-videopress-pkg',
+			)
+		);
+		Assets::enqueue_script( self::JETPACK_VIDEOPRESS_PKG_NAMESPACE );
+
+		// Initial JS state including JP Connection data.
+		wp_add_inline_script( self::JETPACK_VIDEOPRESS_PKG_NAMESPACE, Connection_Initial_State::render(), 'before' );
+		wp_add_inline_script( self::JETPACK_VIDEOPRESS_PKG_NAMESPACE, self::render_initial_state(), 'before' );
+	}
+
+	/**
+	 * Render the initial state into a JavaScript variable.
+	 *
+	 * @return string
+	 */
+	public static function render_initial_state() {
+		return 'var jetpackVideopressInitialState=JSON.parse(decodeURIComponent("' . rawurlencode( wp_json_encode( self::initial_state() ) ) . '"));';
+	}
+
+	/**
+	 * Get the initial state data for hydrating the React UI.
+	 *
+	 * @return array
+	 */
+	public static function initial_state() {
+		return array(
+			'apiRoot'           => esc_url_raw( rest_url() ),
+			'apiNonce'          => wp_create_nonce( 'wp_rest' ),
+			'registrationNonce' => wp_create_nonce( 'jetpack-registration-nonce' ),
+		);
+	}
+
+	/**
+	 * Register the VideoPress block editor block
+	 *
+	 * @return void
+	 */
+	public static function register_videopress_block() {
+		register_block_type( __DIR__ . '/client/block-editor/blocks/videopress/' );
+	}
+}
---
+++ projects/plugins/videopress/src/class-jetpack-videopress-plugin.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Primary class file for the Jetpack VideoPress plugin.
+ *
+ * @package automattic/jetpack-videopress-plugin-plugin
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+use Automattic\Jetpack\Admin_UI\Admin_Menu;
+use Automattic\Jetpack\Connection\Manager as Connection_Manager;
+use Automattic\Jetpack\Connection\Rest_Authentication as Connection_Rest_Authentication;
+use Automattic\Jetpack\My_Jetpack\Initializer as My_Jetpack_Initializer;
+use Automattic\Jetpack\Sync\Data_Settings;
+use Automattic\Jetpack\VideoPress\Initializer as VideoPress_Pkg_Initializer;
+
+/**
+ * Class Jetpack_Videopress_Plugin
+ */
+class Jetpack_Videopress_Plugin {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		// Set up the REST authentication hooks.
+		Connection_Rest_Authentication::init();
+
+		// Init VideoPress package.
+		VideoPress_Pkg_Initializer::init();
+
+		$page_suffix = Admin_Menu::add_menu(
+			__( 'Jetpack VideoPress', 'jetpack-videopress' ),
+			_x( 'VideoPress', 'The Jetpack VideoPress product name, without the Jetpack prefix', 'jetpack-videopress' ),
+			'manage_options',
+			'jetpack-videopress',
+			array( $this, 'plugin_settings_page' ),
+			99
+		);
+		add_action( 'load-' . $page_suffix, array( $this, 'admin_init' ) );
+
+		// Init Jetpack packages and ConnectionUI.
+		add_action(
+			'plugins_loaded',
+			function () {
+				$config = new Automattic\Jetpack\Config();
+				// Connection package.
+				$config->ensure(
+					'connection',
+					array(
+						'slug'     => JETPACK_VIDEOPRESS_SLUG,
+						'name'     => JETPACK_VIDEOPRESS_NAME,
+						'url_info' => JETPACK_VIDEOPRESS_URI,
+					)
+				);
+				// Sync package.
+				$config->ensure( 'sync', Data_Settings::MUST_SYNC_DATA_SETTINGS );
+
+				// Identity crisis package.
+				$config->ensure( 'identity_crisis' );
+
+				$config->ensure( 'videopress' );
+			},
+			1
+		);
+
+		// Register VideoPress block
+		add_action( 'init', array( $this, 'register_videopress_block' ) );
+
+		My_Jetpack_Initializer::init();
+	}
+
+	/**
+	 * Initialize the admin resources.
+	 */
+	public function admin_init() {
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
+	}
+
+	/**
+	 * Enqueue plugin admin scripts and styles.
+	 */
+	public function enqueue_admin_scripts() {
+		VideoPress_Pkg_Initializer::enqueue_admin_scripts();
+	}
+
+	/**
+	 * Main plugin settings page.
+	 */
+	public function plugin_settings_page() {
+		?>
+			<div id="jetpack-videopress-root"></div>
+		<?php
+	}
+
+	/**
+	 * Removes plugin from the connection manager
+	 * If it's the last plugin using the connection, the site will be disconnected.
+	 *
+	 * @access public
+	 * @static
+	 */
+	public static function plugin_deactivation() {
+		$manager = new Connection_Manager( 'jetpack-videopress' );
+		$manager->remove_connection();
+	}
+
+	/**
+	 * Register the VideoPress block.
+	 */
+	public function register_videopress_block() {
+		VideoPress_Pkg_Initializer::register_videopress_block();
+	}
+}

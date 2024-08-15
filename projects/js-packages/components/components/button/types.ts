3b2a24894281fa6fb978669ba81a59a3d1ac5bad
DAVID OKEAMAH <whitneykiss741@gmail.com>
2024-08-15 04:56:29 +0100

    Initialize super-cache/branch-1.8

---
+++ projects/js-packages/components/components/button/types.ts
@@ -0,0 +1,22 @@
+import { Button } from '@wordpress/components';
+import type React from 'react';
+
+type JetpackButtonBaseProps = {
+	className?: string;
+	children?: React.ReactNode;
+	disabled?: boolean;
+	isDestructive?: boolean;
+	isLoading?: boolean;
+	isExternalLink?: boolean;
+	size?: 'normal' | 'small';
+	text?: string;
+	variant?: 'primary' | 'secondary' | 'link';
+	weight?: 'bold' | 'regular';
+	fullWidth?: boolean;
+};
+
+type JetpackLinkProps = Omit< Button.AnchorProps, 'size' | 'variant' >;
+
+type JetpackButtonProps = Omit< Button.ButtonProps, 'size' | 'variant' >;
+
+export type ButtonProps = JetpackButtonBaseProps & ( JetpackLinkProps | JetpackButtonProps );

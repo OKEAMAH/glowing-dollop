6bae02df1f357e1ad005d3ea62ca3f2c5847abf5
DAVID OKEAMAH <whitneykiss741@gmail.com>
2024-08-15 01:19:06 +0100

    Initialize fix/14269

---
+++ extensions/blocks/eventbrite/save.js
@@ -0,0 +1,89 @@
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+import { RichText, getColorClassName } from '@wordpress/block-editor';
+
+/**
+ * Internal dependencies
+ */
+import { createWidgetId } from './utils';
+
+/**
+ * Adapted button save function from @wordpress/block-library
+ * (Using Gutenberg code that shipped with WordPress 5.3)
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/wp/5.3/packages/block-library/src/button/save.js
+ *
+ * Uses a "button" element rather than "a", since the button opens a modal rather than
+ * an external link.
+ */
+
+function saveButton( eventId, attributes ) {
+	const {
+		backgroundColor,
+		borderRadius,
+		customBackgroundColor,
+		customTextColor,
+		text,
+		textColor,
+		url,
+	} = attributes;
+
+	const textClass = getColorClassName( 'color', textColor );
+	const backgroundClass = getColorClassName( 'background-color', backgroundColor );
+
+	const buttonClasses = classnames( 'wp-block-button__link', {
+		'has-text-color': textColor || customTextColor,
+		[ textClass ]: textClass,
+		'has-background': backgroundColor || customBackgroundColor,
+		[ backgroundClass ]: backgroundClass,
+		'no-border-radius': borderRadius === 0,
+	} );
+
+	const buttonStyle = {
+		backgroundColor: backgroundClass ? undefined : customBackgroundColor,
+		color: textClass ? undefined : customTextColor,
+		borderRadius: borderRadius ? borderRadius + 'px' : undefined,
+	};
+
+	return (
+		<div>
+			<RichText.Content
+				id={ createWidgetId( eventId ) }
+				tagName="button"
+				className={ buttonClasses }
+				style={ buttonStyle }
+				value={ text }
+				type="button"
+			/>
+			{ url && (
+				<a className="eventbrite__direct-link" href={ url }>
+					{ url }
+				</a>
+			) }
+		</div>
+	);
+}
+
+export default function save( { attributes } ) {
+	const { eventId, useModal, url } = attributes;
+
+	if ( ! eventId ) {
+		return;
+	}
+
+	if ( useModal ) {
+		return saveButton( eventId, attributes );
+	}
+
+	return (
+		<div id={ createWidgetId( eventId ) }>
+			{ url && (
+				<a className="eventbrite__direct-link" href={ url }>
+					{ url }
+				</a>
+			) }
+		</div>
+	);
+}

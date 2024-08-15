0b3f6bfda5e779ae02038361025ff49b26f6d2a6
DAVID OKEAMAH <whitneykiss741@gmail.com>
2024-08-15 04:15:17 +0100

    Initialize dnm/test-update/lock-approvals

---
+++ projects/packages/waf/cli.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Registers the CLI functionality.
+ *
+ * @package automattic/jetpack-waf
+ */
+
+namespace Automattic\Jetpack\Waf;
+
+if ( defined( 'WP_CLI' ) && \WP_CLI ) {
+	\WP_CLI::add_command( 'jetpack-waf', CLI::class );
+}
+
+echo 'CLI loaded';

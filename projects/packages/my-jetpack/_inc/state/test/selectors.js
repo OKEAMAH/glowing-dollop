463c85b2079e3a0160caac14dc5dad4520fbdfad
DAVID OKEAMAH <whitneykiss741@gmail.com>
2024-08-15 04:35:00 +0100

    Initialize change/jetpack-social-admin-page-share-designs

---
+++ projects/packages/my-jetpack/_inc/state/test/selectors.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import selectors from '../selectors';
+
+describe( 'requests selectors', () => {
+	describe( '#isRequestingPurchases', () => {
+		it( 'should return False when no state', () => {
+			const state = {};
+			const output = selectors.isRequestingPurchases( state );
+			expect( output ).to.equal( false );
+		} );
+
+		it( 'should return False when no requesting according to state tree', () => {
+			const state = {
+				isRequestingPurchases: false,
+			};
+
+			const output = selectors.isRequestingPurchases( state );
+			expect( output ).to.equal( false );
+		} );
+
+		it( 'should return True when requesting according to state tree', () => {
+			const state = {
+				isRequestingPurchases: true,
+			};
+
+			const output = selectors.isRequestingPurchases( state );
+			expect( output ).to.equal( true );
+		} );
+	} );
+} );
---
+++ projects/plugins/jetpack/extensions/blocks/pinterest/test/edit-url-form.js
@@ -0,0 +1,56 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import EditUrlForm from '../components/edit-url-form';
+
+describe( 'EditUrlForm', () => {
+	const onSubmit = jest.fn();
+	const setUrl = jest.fn();
+	const defaultProps = {
+		className: 'blame-it-on-the-enchiladas',
+		onSubmit,
+		setUrl,
+		noticeUI: [ <p key="hi">hi!</p> ],
+		url: 'https://get.your/art/to-mars',
+	};
+
+	beforeEach( () => {
+		onSubmit.mockClear();
+		setUrl.mockClear();
+	} );
+
+	test( 'should render', () => {
+		render( <EditUrlForm { ...defaultProps } /> );
+		const { container } = render( <EditUrlForm { ...defaultProps } /> );
+		expect( container.querySelector( 'div' ).className ).toContain( defaultProps.className );
+	} );
+
+	test( 'defaults value to url', () => {
+		render( <EditUrlForm { ...defaultProps } /> );
+
+		expect( screen.getByLabelText( 'Pinterest URL' ).value ).toEqual( defaultProps.url );
+	} );
+
+	test( 'calls onSubmit when submitting form', () => {
+		const { container } = render( <EditUrlForm { ...defaultProps } /> );
+		const form = container.querySelector( 'form' );
+		fireEvent.submit( form );
+
+		expect( onSubmit ).toHaveBeenCalled();
+	} );
+
+	test( 'calls setUrl when updating input field', async () => {
+		const user = userEvent.setup();
+		render( <EditUrlForm { ...defaultProps } /> );
+		await user.type( screen.getByLabelText( 'Pinterest URL' ), 'blah' );
+
+		expect( setUrl ).toHaveBeenCalledTimes( 4 );
+	} );
+
+} );
+

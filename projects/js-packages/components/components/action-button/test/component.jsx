0bf1d614f14bc82e05076d396656b63c57d38605
DAVID OKEAMAH <whitneykiss741@gmail.com>
2024-08-15 04:43:00 +0100

    Initialize social/post-release-1.1.0

---
+++ projects/js-packages/components/components/action-button/test/component.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ActionButton from '../index';
+
+describe( 'ActionButton', () => {
+	const testProps = {
+		label: 'Action!',
+	};
+
+	describe( 'Render the ActionButton component', () => {
+		render( <ActionButton { ...testProps } /> );
+
+		it( 'renders the register button', () => {
+			expect( screen.getByRole( 'button' ) ).toHaveTextContent( 'Action!' );
+		} );
+	} );
+} );
---
+++ projects/js-packages/components/components/spinner/test/component.jsx
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import Spinner from '../index';
+
+describe( 'Spinner', () => {
+	const testProps = {
+		className: 'sample-classname',
+	};
+
+	describe( 'Render the Spinner component', () => {
+		const { container } = render( <Spinner { ...testProps } /> );
+
+		it( 'validate the class name', () => {
+			// eslint-disable-next-line testing-library/no-node-access
+			expect( container.firstChild ).toHaveClass( 'sample-classname' );
+		} );
+	} );
+} );

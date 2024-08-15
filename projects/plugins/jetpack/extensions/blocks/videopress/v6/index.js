4e0fa8542c3f632c8c60c537140e31b921756796
DAVID OKEAMAH <whitneykiss741@gmail.com>
2024-08-15 04:48:43 +0100

    Initialize update/jetpack-vpblock-convert-core-video-to-vpblock

---
+++ projects/plugins/jetpack/extensions/blocks/videopress/v6/index.js
@@ -0,0 +1,54 @@
+/**
+ * WordPress dependencies
+ */
+import { createBlock } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+/**
+ * Internal dependencies
+ */
+import attributes from './attributes';
+import { VideoPressIcon as icon } from './components/icons';
+import deprecatedV0 from './deprecated/v0';
+import edit from './edit';
+import save from './save';
+import './style.scss';
+
+export const name = 'videopress-block';
+export const namespace = 'jetpack';
+export const blockType = `${ namespace }/${ name }`;
+
+export const title = __( 'VideoPress', 'jetpack' );
+export const description = __(
+	'Embed a video from your media library or upload a new one with VideoPress.',
+	'jetpack'
+);
+
+export const settings = {
+	title,
+	description,
+	icon,
+	category: 'media',
+	edit,
+	save,
+	attributes,
+	supports: {
+		align: true,
+	},
+	transforms: {
+		from: [
+			{
+				type: 'block',
+				blocks: [ 'core/video' ],
+				transform: attrs => createBlock( 'jetpack/videopress-block', attrs ),
+			},
+		],
+		to: [
+			{
+				type: 'block',
+				blocks: [ 'core/video' ],
+				transform: attrs => createBlock( 'core/video', attrs ),
+			},
+		],
+	},
+	deprecated: [ deprecatedV0 ],
+};

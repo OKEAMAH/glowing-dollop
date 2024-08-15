84463135e3ef5d84b0d5c0dd7a5b819774071576
DAVID OKEAMAH <whitneykiss741@gmail.com>
2024-08-15 02:57:32 +0100

    Initialize experiment/jp-search-translations

---
+++ projects/packages/connection-ui/_inc/components/admin/index.jsx
@@ -0,0 +1,102 @@
+/**
+ * External dependencies
+ */
+import React, { useCallback } from 'react';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
+import {
+	ConnectionStatusCard,
+	ConnectScreen,
+	withConnectionStatus,
+} from '@automattic/jetpack-connection';
+import { IDCScreen } from '@automattic/jetpack-idc';
+
+/**
+ * Internal dependencies
+ */
+import { STORE_ID } from '../../store';
+import Header from '../header';
+import './style.scss';
+import ConnectRight from './assets/connect-right.png';
+
+const ConnectScreenWithConnectionStatus = withConnectionStatus( ConnectScreen );
+
+/**
+ * The Connection IU Admin App.
+ *
+ * @returns {object} The Admin component.
+ */
+export default function Admin() {
+	const APINonce = useSelect( select => select( STORE_ID ).getAPINonce(), [] );
+	const APIRoot = useSelect( select => select( STORE_ID ).getAPIRoot(), [] );
+	const registrationNonce = useSelect( select => select( STORE_ID ).getRegistrationNonce(), [] );
+	const assetBuildUrl = useSelect( select => select( STORE_ID ).getAssetBuildUrl(), [] );
+
+	const connectionStatus = useSelect( select => select( STORE_ID ).getConnectionStatus(), [] );
+	const { setConnectionStatus } = useDispatch( STORE_ID );
+
+	// Placeholder for testing purposes.
+	const hasIDC = true;
+	const IDCHomeUrl = 'https://site1.local/';
+	const currentUrl = 'https://site2.local/';
+
+	const statusCallback = useCallback(
+		status => {
+			setConnectionStatus( status );
+		},
+		[ setConnectionStatus ]
+	);
+
+	const onDisconnectedCallback = useCallback( () => {
+		setConnectionStatus( { isActive: false, isRegistered: false, isUserConnected: false } );
+	}, [ setConnectionStatus ] );
+
+	if ( hasIDC ) {
+		return <IDCScreen wpcomHomeUrl={ IDCHomeUrl } currentUrl={ currentUrl } />;
+	}
+
+	return (
+		<React.Fragment>
+			<Header />
+
+			{ connectionStatus.isRegistered && (
+				<ConnectionStatusCard
+					isRegistered={ connectionStatus.isRegistered }
+					isUserConnected={ connectionStatus.isUserConnected }
+					apiRoot={ APIRoot }
+					apiNonce={ APINonce }
+					onDisconnected={ onDisconnectedCallback }
+					redirectUri="tools.php?page=wpcom-connection-manager"
+				/>
+			) }
+
+			{ ! connectionStatus.isRegistered && (
+				<ConnectScreenWithConnectionStatus
+					apiRoot={ APIRoot }
+					apiNonce={ APINonce }
+					registrationNonce={ registrationNonce }
+					from="connection-ui"
+					redirectUri="tools.php?page=wpcom-connection-manager"
+					statusCallback={ statusCallback }
+					images={ [ ConnectRight ] }
+					assetBaseUrl={ assetBuildUrl }
+				>
+					<p>
+						{ __(
+							"Secure and speed up your site for free with Jetpack's powerful WordPress tools.",
+							'jetpack'
+						) }
+					</p>
+
+					<ul>
+						<li>{ __( 'Measure your impact with beautiful stats', 'jetpack' ) }</li>
+						<li>{ __( 'Speed up your site with optimized images', 'jetpack' ) }</li>
+						<li>{ __( 'Protect your site against bot attacks', 'jetpack' ) }</li>
+						<li>{ __( 'Get notifications if your site goes offline', 'jetpack' ) }</li>
+						<li>{ __( 'Enhance your site with dozens of other features', 'jetpack' ) }</li>
+					</ul>
+				</ConnectScreenWithConnectionStatus>
+			) }
+		</React.Fragment>
+	);
+}
---
+++ projects/plugins/jetpack/tools/webpack.helpers.js
@@ -0,0 +1,79 @@
+/**
+ * External dependencies
+ */
+const webpack = require( 'webpack' );
+const fs = require( 'fs' );
+const path = require( 'path' );
+
+/**
+ * Returns an instance of the DefinePlugin that adds color-studio colors as literals.
+ *
+ * @returns {object} DefinePlugin instance.
+ */
+function definePaletteColorsAsStaticVariables() {
+	return new webpack.DefinePlugin( {
+		// Replace palette colors as individual literals in the bundle.
+		PALETTE: ( () => {
+			const colors = require( '@automattic/color-studio' ).colors;
+			const stringifiedColors = {};
+
+			// DefinePlugin replaces the values as unescaped text.
+			// We therefore need to double-quote each value, to ensure it ends up as a string.
+			for ( const color in colors ) {
+				stringifiedColors[ color ] = `"${ colors[ color ] }"`;
+			}
+
+			return stringifiedColors;
+		} )(),
+	} );
+}
+
+/**
+ * Returns an instance of the AddReadableJsAssetsWebpackPlugin that adds readable JS assets.
+ *
+ * 1. For translation strings extraction - for now, `.min.js` files are excluded.
+ * 2. It also removes contentHash/hash from the chunk file name, which makes it possible for
+ * PHP easier to inline translations - and this is essential for WPCOM, as we deploy whenever
+ * a PR is merged.
+ *
+ * @returns {object} AddReadableJsAssetsWebpackPlugin instance.
+ */
+function defineReadableJSAssetsPluginForSearch() {
+	return new ( class AddReadableJsAssetsWebpackPlugin {
+		extractUnminifiedFiles( compilation ) {
+			const files = Array.from( compilation.chunks ).flatMap( chunk => Array.from( chunk.files ) );
+			compilation.unminifiedAssets = files
+				.map( file => {
+					const asset = compilation.assets[ file ];
+					// Remove the hash in chunk file names for the sake of translations loading from PHP.
+					// Length of hash has to be 16 or longer, the default value is 20.
+					const unminifiedFile = file.replace( /(-[a-z0-9]{20})?\.min\.js$/, '.js' );
+					return [ unminifiedFile, asset.source() ];
+				} )
+				.filter( val => val );
+		}
+		async writeUnminifiedFiles( compilation ) {
+			for ( const [ file, source ] of compilation.unminifiedAssets ) {
+				await fs.promises.writeFile(
+					path.join( compilation.options.output.path, file ),
+					source.replace( /\r\n/g, '\n' )
+				);
+			}
+		}
+		apply( compiler ) {
+			compiler.hooks.compilation.tap( this.constructor.name, compilation => {
+				compilation.hooks.additionalAssets.tap( this.constructor.name, () =>
+					this.extractUnminifiedFiles( compilation )
+				);
+			} );
+			compiler.hooks.afterEmit.tapPromise( this.constructor.name, compilation =>
+				this.writeUnminifiedFiles( compilation )
+			);
+		}
+	} )();
+}
+
+module.exports = {
+	definePaletteColorsAsStaticVariables,
+	defineReadableJSAssetsPluginForSearch,
+};

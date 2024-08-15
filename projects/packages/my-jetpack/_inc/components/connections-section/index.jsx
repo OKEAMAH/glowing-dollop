83eb0c54d82dc64b24a4a0bed1938c63096bdb4e
DAVID OKEAMAH <whitneykiss741@gmail.com>
2024-08-15 03:33:55 +0100

    Initialize update/my-jetpack-introduce-wordpress-element

---
+++ projects/packages/my-jetpack/_inc/components/connections-section/index.jsx
@@ -0,0 +1,29 @@
+/* global myJetpackRest */
+/* global myJetpackInitialState */
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { useCallback } from '@wordpress/element';
+import { ConnectionStatusCard } from '@automattic/jetpack-connection';
+
+/**
+ * Plan section component.
+ *
+ * @returns {object} ConnectionsSection React component.
+ */
+export default function ConnectionsSection() {
+	const { apiRoot, apiNonce } = myJetpackRest;
+	const redirectAfterDisconnect = useCallback( () => {
+		window.location = myJetpackInitialState.topJetpackMenuItemUrl;
+	}, [] );
+
+	return (
+		<ConnectionStatusCard
+			apiRoot={ apiRoot }
+			apiNonce={ apiNonce }
+			redirectUri={ myJetpackInitialState.redirectUri }
+			onDisconnected={ redirectAfterDisconnect }
+		/>
+	);
+}

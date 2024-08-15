b5981f4ad4e5cdc18681248159305b858454c5bd
DAVID OKEAMAH <whitneykiss741@gmail.com>
2024-08-15 03:58:30 +0100

    Initialize update/videopress-add-compat-format

---
+++ projects/plugins/boost/app/assets/src/js/pages/settings/sections/Modules.svelte
@@ -0,0 +1,92 @@
+<script>
+	/**
+	 * Internal dependencies
+	 */
+	import { maybeGenerateCriticalCss } from '../../../utils/generate-critical-css';
+	import { requestCloudCss } from '../../../utils/cloud-css';
+	import RefreshIcon from '../../../svg/refresh.svg';
+	import GenerateCss from '../elements/GenerateCSS.svelte';
+	import Module from '../elements/Module.svelte';
+	import TemplatedString from '../../../elements/TemplatedString.svelte';
+	import externalLinkTemplateVar from '../../../utils/external-link-template-var';
+
+	/**
+	 * WordPress dependencies
+	 */
+	import { __ } from '@wordpress/i18n';
+</script>
+
+<div class="jb-container--narrow">
+	<Module
+		slug={'critical-css'}
+		on:enabled={maybeGenerateCriticalCss}
+		on:mountEnabled={maybeGenerateCriticalCss}
+	>
+		<h3 slot="title">
+			{__( 'Optimize CSS Loading', 'jetpack-boost' )}
+		</h3>
+		<p slot="description">
+			<TemplatedString
+				template={__(
+					`Move important styling information to the start of the page, which helps pages display your content sooner, so your users don’t have to wait for the entire page to load. Commonly referred to as <link>Critical CSS</link>.`,
+					'jetpack-boost'
+				)}
+				vars={externalLinkTemplateVar( 'https://web.dev/extract-critical-css/' )}
+			/>
+		</p>
+
+		<div slot="meta">
+			<GenerateCss />
+		</div>
+	</Module>
+
+	<Module slug={'cloud-css'} on:enabled={requestCloudCss}>
+		<h3 slot="title">
+			{__( 'Optimize CSS Loading from Cloud', 'jetpack-boost' )}
+		</h3>
+		<p slot="description">
+			<TemplatedString
+				template={__(
+					`Move important styling information to the start of the page, which helps pages display your content sooner, so your users don’t have to wait for the entire page to load. Commonly referred to as <link>Critical CSS</link>.`,
+					'jetpack-boost'
+				)}
+				vars={externalLinkTemplateVar( 'https://web.dev/extract-critical-css/' )}
+			/>
+		</p>
+
+		<div slot="meta">
+			<button type="button" class="components-button is-link" on:click={requestCloudCss}>
+				<RefreshIcon />
+				{__( 'Regenerate', 'jetpack-boost' )}
+			</button>
+		</div>
+	</Module>
+
+	<Module slug={'render-blocking-js'}>
+		<h3 slot="title">
+			{__( 'Defer Non-Essential JavaScript', 'jetpack-boost' )}
+		</h3>
+		<p slot="description">
+			<TemplatedString
+				template={__(
+					`Run non-essential JavaScript after the page has loaded so that styles and images can load more quickly. Read more on <link>web.dev</link>.`,
+					'jetpack-boost'
+				)}
+				vars={externalLinkTemplateVar( 'https://web.dev/efficiently-load-third-party-javascript/' )}
+			/>
+		</p>
+	</Module>
+
+	<Module slug={'lazy-images'}>
+		<h3 slot="title">{__( 'Lazy Image Loading', 'jetpack-boost' )}</h3>
+		<p slot="description">
+			<TemplatedString
+				template={__(
+					`Improve page loading speed by only loading images when they are required. Read more on <link>web.dev</link>.`,
+					'jetpack-boost'
+				)}
+				vars={externalLinkTemplateVar( 'https://web.dev/browser-level-image-lazy-loading/' )}
+			/>
+		</p>
+	</Module>
+</div>

09b66ac305f434bc7fb2e107cbf7992bb15c8d96
DAVID OKEAMAH <whitneykiss741@gmail.com>
2024-08-15 03:56:31 +0100

    Initialize feat/shared-js-plugin-move-jetpack-plugin-sidebar

---
+++ projects/packages/my-jetpack/_inc/components/connections-section/index.jsx
@@ -0,0 +1,32 @@
+/* global myJetpackInitialState */
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { ConnectionStatusCard } from '@automattic/jetpack-connection';
+
+/**
+ * Internal dependencies
+ */
+import useMyJetpackConnection from '../../hooks/use-my-jetpack-connection';
+import useMyJetpackNavigate from '../../hooks/use-my-jetpack-navigate';
+
+/**
+ * Plan section component.
+ *
+ * @returns {object} ConnectionsSection React component.
+ */
+export default function ConnectionsSection() {
+	const { apiRoot, apiNonce, redirectUrl } = useMyJetpackConnection();
+	const navigate = useMyJetpackNavigate( '/connection' );
+	const { connectedPlugins } = myJetpackInitialState;
+	return (
+		<ConnectionStatusCard
+			apiRoot={ apiRoot }
+			apiNonce={ apiNonce }
+			redirectUri={ redirectUrl }
+			onConnectUser={ navigate }
+			connectedPlugins={ connectedPlugins }
+		/>
+	);
+}
